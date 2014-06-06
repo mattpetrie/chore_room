03817308268ec1a2391dfd9439ad2eeda1e99bdc
Nathan Hsieh <hsieh.nathan@gmail.com>
2014-06-06 16:40:38 -0700

    reorganized some methods into partials, added addChores form

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -6,5 +6,30 @@ class ChoresController < ApplicationController
     @chores = Chore.all
     @users = User.all
   end
+
+  def new
+  end
   
+  def create
+    @chore = Chore.new(chore_params)
+    if @chore.save
+      render json: @chore
+    else
+      render json: @chore.errors.full_messages, status: 422
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  def chore_params
+    params.require(:chore).permit(:title, :user_id, :due_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do  
   root to: 'chores#index' 
   resource :session, only: :new
+  resources :chores
   
   get "/auth/github/callback", to: "sessions#create"
   delete "/signout", to: "sessions#destroy", as: "signout"
