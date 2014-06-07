8b26af8b78cd2dac87f0e71a81afd0bfb32ae4a4
Matt Petrie <matthew.petrie@gmail.com>
2014-06-07 15:34:12 -0700

    merging changes

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -4,23 +4,21 @@
 #
 #  id         :integer          not null, primary key
 #  title      :string(255)      not null
-#  user_id    :integer          not null
-#  due_date   :datetime         not null
 #  completed  :boolean          default(FALSE), not null
 #  created_at :datetime
 #  updated_at :datetime
 #
 
 class Chore < ActiveRecord::Base
-  validates :title, :user, :due_date, presence: true
+  validates :title, presence: true
 
   belongs_to :user
 
-  def self.assign_chores!(user_ids)
-    user_list = user_ids.shuffle!
-    user_list += user_list.shuffle! until user_list.length >= Chore.count
-    self.update_all(completed: false)  
-    self.all.each { |chore| chore.user_id = user_list.shift }
-  end
+  # Refactor to work with ChoreAssignments
+  # def self.assign_chores!(user_ids)
+  #   user_list = user_ids.shuffle!
+  #   user_list += user_list.shuffle! until user_list.length >= Chore.count
+  #   self.update_all(completed: false)
+  #   self.all.each { |chore| chore.user_id = user_list.shift }
+  # end
 end
-
