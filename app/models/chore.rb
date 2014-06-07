3be1e5602ef7946dc8efe7880c1959d59fa8b737
Robert Honsby <rhonsby@gmail.com>
2014-06-07 15:13:14 -0700

    Add admin column to users

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -4,22 +4,22 @@
 #
 #  id         :integer          not null, primary key
 #  title      :string(255)      not null
-#  user_id    :integer          not null
-#  due_date   :datetime         not null
+#  user_id    :integer
+#  due_date   :datetime
 #  completed  :boolean          default(FALSE), not null
 #  created_at :datetime
 #  updated_at :datetime
 #
 
 class Chore < ActiveRecord::Base
-  validates :title, :user, :due_date, presence: true
+  validates :title, presence: true
 
   belongs_to :user
 
   def self.assign_chores!(user_ids)
     user_list = user_ids.shuffle!
     user_list += user_list.shuffle! until user_list.length >= Chore.count
-    self.update_all(completed: false)  
+    self.update_all(completed: false)
     self.all.each { |chore| chore.user_id = user_list.shift }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607204620) do
+ActiveRecord::Schema.define(version: 20140607221138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,13 @@ ActiveRecord::Schema.define(version: 20140607204620) do
   add_index "chores", ["user_id"], name: "index_chores_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "name",       null: false
-    t.string   "uid",        null: false
-    t.string   "email",      null: false
-    t.string   "photo",      null: false
+    t.string   "name",                       null: false
+    t.string   "uid",                        null: false
+    t.string   "email",                      null: false
+    t.string   "photo",                      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",      default: false, null: false
   end
 
   add_index "users", ["uid"], name: "index_users_on_uid", unique: true, using: :btree
